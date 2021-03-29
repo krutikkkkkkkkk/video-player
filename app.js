d1681f5b4f5a22d53b5ddb6f0578c0bc8166867f
Krutik Raut <58563133+reboot13-git@users.noreply.github.com>
2021-03-29 13:03:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ function play() {
 
         var url = document.getElementById('url').value;
         url = url.replace("https://youtu.be/","https://www.youtube.com/embed/");
-        console.log(u)
+        console.log(url)
         url = url.replace("https://www.youtube.com/watch?v=","https://www.youtube.com/embed/");
         console.log(url)
         var iframe = document.getElementById('iframe');
