5e9257b399ebf4c59e8d59883d578eb208c27b5e
Krutik Raut <58563133+reboot13-git@users.noreply.github.com>
2021-03-29 12:43:23 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+function play() {
+
+        var url = document.getElementById('url').value;
+        url = url.replace("https://youtu.be/","https://www.youtube.com/embed/");
+        console.log(u)
+        url = url.replace("https://www.youtube.com/watch?v=","https://www.youtube.com/embed/");
+        console.log(url)
+        var iframe = document.getElementById('iframe');
+        var src = document.createAttribute("src");
+        src.value = url;
+        iframe.setAttributeNode(src);
+    }
